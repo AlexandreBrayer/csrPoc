bf08bba1ea83c8c4c3efa3b17e437f72ca14ca00
Alexandre Brayer <a.brayer@purjus.fr>
2024-07-24 10:35:48 +0200

    first commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+
+export const csr = false;
+
+const schema = z.object({
+	result: z.string().regex(/^[-+]?\d*\.?\d+$/, 'Result must be a valid number')
+});
+
+export const load = async () => {
+	const form = await superValidate(zod(schema));
+	return { form };
+};
+
+import { message } from 'sveltekit-superforms';
+import { fail } from '@sveltejs/kit';
+
+export const actions = {
+	default: async ({ request }) => {
+		const form = await superValidate(request, zod(schema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		if (form.data.result === '50') {
+			return message(form, 'Correct!');
+		}
+
+		return message(form, 'Incorrect!');
+	}
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+
+	export let data;
+
+	const { form, message, errors } = superForm(data.form);
+</script>
+
+<form method="POST">
+	<label for="result">5 x 10 ?</label>
+	<input type="text" id="result" name="result" placeholder="réponse" bind:value={$form.result} />
+	{#if $errors.result}<span class="invalid">{$errors.result}</span>{/if}
+	<button type="submit">Submit</button>
+</form>
+
+{#if $message}
+	<p>{$message}</p>
+{/if}
+
+<style>
+	.invalid {
+		color: red;
+	}
+</style>
