cae282c0d938ce1688fb9b41c13c9a7fd2fe7c05
Alexandre Brayer <a.brayer@purjus.fr>
2024-07-24 12:36:49 +0200

    keep data poc

--- src/routes/division/+page.svelte
+++ src/routes/division/+page.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <form method="POST">
-	<label for="result">5 x 10 ?</label>
+	<label for="result">10 / 2 ?</label>
 	<input type="text" id="result" name="result" placeholder="réponse" bind:value={$form.result} />
 	{#if $errors.result}<span class="invalid">{$errors.result}</span>{/if}
 	<button type="submit">Submit</button>
